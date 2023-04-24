6fe42d4580301f639310c891830f791fba7260a2
Sulhwa-Mia-Choi <81615048+Sulhwa-Mia-Choi@users.noreply.github.com>
2023-04-24 13:59:34 +0900

    UserStorage model 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -28,6 +28,7 @@ function login() {
     })
         .then((res) => res.json())
         .then((res) => {
+            console.log(res);
             if (res.success) {
                 location.href = "/";
             } else {
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,6 @@
 
+const UserStorage = require("../../models/UserStorage");
+
 const output = {
     home : (req, res)=>{
         res.render("home/index");
@@ -9,32 +11,35 @@ const output = {
 
 }
 
-const users = {
-    id : ["woorimIT","나개발","김팀장"],
-    psword : ["1234","1234","123456"],
-};
+
 
 const process = {
     login : (req, res) => {
         const id = req.body.id;
         const psword = req.body.psword;
+        const response = {}; // return할 객체
 
-        console.log(id)
-        console.log(psword);
+        //const userStorage = new UserStorage();
+        //console.log(userStorage.users);
+        // 객체생성을 안하고 밑에처럼 바로 가져다 쓰면 undefined가 뜨기때문에
+        // model 객체에 static을 넣어줘야함
+        const users = UserStorage.getUsers("id","psword"); 
+
+        
+
+
 
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
             if (users.psword[idx] === psword) {
-                return res.json({
-                    success:true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
 
-        return res.json({
-            success:false,
-            msg:"로그인 실패",
-        })
+        response.success = false;
+        response.msg = "로그인 실패";
+        return res.json(response);
     },
 }
 
